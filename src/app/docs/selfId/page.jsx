94e464b0a5818687db8430e77f98d6478d040868
immoktarulislam <moktarul888@gmail.com>
2026-09-18 19:34:59 +0530

    navbar dynamic task

--- src/app/docs/selfId/page.jsx
+++ src/app/docs/selfId/page.jsx
@@ -6,6 +6,7 @@ const SelfIdPage = () => {
             <a href="">voter ID</a><br />
             <a href="">aadhar Card</a><br />
             <a href="">drivering licence</a>
+            <h2>this is my log</h2>
         </div>
     );
 };
